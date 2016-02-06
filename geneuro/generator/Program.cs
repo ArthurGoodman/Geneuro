eae614047eca93e787c4cf424cb7189f53e1a68f
ArthurGoodman <arthur.goodman3@gmail.com>
2016-02-06 20:23:19 +0200

    generator

---
+++ geneuro/generator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace generator {
+    class Program {
+        private const int Width = 50;
+        private const int Height = 50;
+
+        private static Settings settings;
+
+        static void Main(string[] args) {
+            try {
+                settings = new Settings(args);
+                Generate();
+            } catch (Exception e) {
+                Console.WriteLine("error: " + e.Message);
+            }
+        }
+
+        private static void Generate() {
+            for (char c = settings.FirstChar; c <= settings.LastChar; c++)
+                Directory.CreateDirectory("out/" + c);
+
+            Brush brush = new SolidBrush(Color.Black);
+
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            format.LineAlignment = StringAlignment.Center;
+
+            for (int i = 0; i < settings.Fonts.Length; i++) {
+                Font font = new Font(settings.Fonts[i], settings.Sizes[i]);
+
+                for (char c = settings.FirstChar; c <= settings.LastChar; c++) {
+                    Bitmap image = new Bitmap(Width, Height);
+
+                    Graphics g = Graphics.FromImage(image);
+                    g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                    g.DrawString(c.ToString(), font, brush, Width / 2, Height / 2, format);
+
+                    image.Save("out/" + c + "/" + i + ".bmp");
+                }
+            }
+        }
+    }
+}
---
+++ geneuro/generator/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace generator {
+    class Settings {
+        public char FirstChar { get; private set; }
+        public char LastChar { get; private set; }
+        public string[] Fonts { get; private set; }
+        public int[] Sizes { get; private set; }
+
+        public static void InvalidArguments() {
+            throw new Exception("invalid arguments");
+        }
+
+        public Settings(string[] args) {
+            if (args.Length != 3 || args[0].Length > 1 || args[1].Length > 1)
+                InvalidArguments();
+
+            FirstChar = args[0][0];
+            LastChar = args[1][0];
+            LoadFonts(args[2]);
+        }
+
+        private void LoadFonts(string fileName) {
+            string[] lines = File.ReadAllLines(fileName);
+            Fonts = new string[lines.Length];
+            Sizes = new int[lines.Length];
+            int i = 0;
+
+            foreach (string line in lines) {
+                if (line.Length == 0)
+                    continue;
+
+                int index = line.LastIndexOf(' ');
+
+                Fonts[i] = line.Substring(0, index);
+                Sizes[i] = int.Parse(line.Substring(index + 1));
+
+                i++;
+            }
+        }
+    }
+}
